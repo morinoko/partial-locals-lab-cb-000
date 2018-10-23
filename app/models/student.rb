4275d72b6f4d46f3987a8c88307b1128ee04e4e4
Felice Forby <feliceforby@gmail.com>
2018-10-23 23:47:05 +0000

    refactor partials and begin working on search

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,9 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(query)
+    query = /#{query}/i
+    self.all.select { |student| student.name =~ query }
+  end
 end
